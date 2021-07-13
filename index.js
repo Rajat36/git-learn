39ac01e21dec4a4226435ae4ca534f60901baffe
Rajat <8bp328@gmail.com>
2021-07-13 17:29:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// var name = 'Utkarsh'
+// console.log('hey')
+
+// const laptop = {
+//   name : 'Asus',
+//   modale :  'X500'
+// }
+
+// console.log(laptop)
+// console.log(laptop.name)
+
+// let language = 'Javascript';
+
+// console.log(language);
+
+const mininsec = 60;
+console.log(mininsec)
+
+{
+  let a = 20;
+  console.log(a)
+}
+
+  // console.log(a)
